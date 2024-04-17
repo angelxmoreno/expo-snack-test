182320df878e5592b78020b3ddf67a4b3226ebbf
Angel S. Moreno <angelxmoreno@gmail.com>
2024-04-17 00:15:11 -0400

    chore: project init

---
+++ App.js
@@ -0,0 +1,19 @@
+import { SafeAreaView, StyleSheet } from 'react-native';
+import SnapchatSocialButton from './components/SnapchatSocialButton'
+
+export default function App() {
+    return (
+        <SafeAreaView style={styles.container}>
+            <SnapchatSocialButton />
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#ecf0f1',
+        padding: 8,
+    }
+});
